49541fe2cbf9168f92a702be3a500c5a2ebdb483
nitexius <109669190+nitexius@users.noreply.github.com>
2022-11-22 11:04:01 +0300

    Delete forms.py

--- forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-
-class KlogicForm(forms.Form):
-    station = forms.CharField(max_length=10, label='Номер станции', required=False)
-    klogic_file = forms.FileField(label='Klogic XML')
-    bd = forms.BooleanField(label='Сформировать Klogger XML', required=False)
-    klogger_file = forms.FileField(label='Klogger XML', required=False)
-    alarm = forms.BooleanField(label='Сформировать Alarms XML', required=False)
