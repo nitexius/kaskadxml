e02d700dc3067d112463e52e03ac64b435936330
nitexius <109669190+nitexius@users.noreply.github.com>
2022-11-22 11:04:27 +0300

    Delete tools directory

--- tools/__init__.py
+++
@@ -1,2 +0,0 @@
-from .file_tools import set_klogic_xml, set_klogger_xml, set_alarms_xml, transform_file, set_arch
-from .update_xmls_tools import update_klogic_xml, update_klogger_xml, update_alarms_xml
