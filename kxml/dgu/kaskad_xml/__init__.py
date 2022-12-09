5d6de182154af80f6d2a57cccf62095f3b03ea57
nitexius <109669190+nitexius@users.noreply.github.com>
2022-12-09 11:59:56 +0300

    Add files via upload

---
+++ kxml/dgu/kaskad_xml/__init__.py
@@ -0,0 +1,5 @@
+from .klogic_xml import KlogicXML
+from .alarms_xml import AlarmsXML
+from .indices import indices as i, constants as c
+from .alrm import alrm
+# from .exceptions import DefaultAlarmError, AlarmsBadFormatError
