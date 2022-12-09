b5c98ebb6ce7b000994da47f56d715544fac0859
nitexius <109669190+nitexius@users.noreply.github.com>
2022-12-09 11:49:10 +0300

    Add files via upload

--- kxml/kaskadxml/kaskad_xml/__init__.py
+++ kxml/kaskadxml/kaskad_xml/__init__.py
@@ -1,6 +1,16 @@
 from .klogger_xml import KloggerXML
-from .klogic_xml import KlogicXML
-from .alarms_xml import AlarmsXML
+from .klogic_xml import (
+    KlogicXML,
+    KlogicAttrs,
+    NewTagAttrs,
+    Tag,
+    get_tag_value_list,
+)
+from .alarms_xml import (
+    AlarmsXML,
+    AlarmTagAttrs,
+    AlarmAttrs,
+)
 from .alrm import alrm, stations, xo_choices
 from .indices import indices as i, constants as c, smart_divide_all_n
 from .exceptions import (
