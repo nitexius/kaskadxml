a931ffadf5f9c54e6db0b98cd2f67ae581c2562f
nitexius <109669190+nitexius@users.noreply.github.com>
2022-12-09 11:53:15 +0300

    Add files via upload

--- kxml/kaskadxml/tools/__init__.py
+++ kxml/kaskadxml/tools/__init__.py
@@ -1,2 +1,12 @@
-from .file_tools import set_klogic_xml, set_klogger_xml, set_alarms_xml, transform_file, set_arch
+from .file_tools import (
+	set_klogic_xml,
+	set_klogger_xml,
+	set_alarms_xml,
+	transform_file,
+	set_arch,
+	get_input_file,
+	set_arch,
+	OutputFiles,
+	create_output_file
+)
 from .update_xmls_tools import update_klogic_xml, update_klogger_xml, update_alarms_xml
--- kxml/kaskadxml/tools/shift_tools.py
+++ kxml/kaskadxml/tools/shift_tools.py
@@ -1,3 +1,27 @@
+# from io import BytesIO
+# from kaskadxml.kaskad_xml import KlogicXML
+#
+#
+# def shift_create(klogic_xml: KlogicXML) -> BytesIO:
+#     """Подсчет смещения адресов контроллеров"""
+#     shift_attr = klogic_xml.shift()
+#     buffer = BytesIO()
+#
+#     for l in shift_attr.all_lens:
+#         address = 0
+#         for i, _ in enumerate(shift_attr.all_attrs):
+#             if shift_attr.all_attrs[i].len_group != l:
+#                 continue
+#             current_shift = (
+#                 float(shift_attr.all_attrs[i].addr) - address
+#                 if i and address else 0
+#             )
+#             address = float(shift_attr.all_attrs[i].addr)
+#             new_str = f'Кол-во переменных = {l}. {shift_attr.all_attrs[i].name}. Смещение = {current_shift} \n'
+#             buffer.write(bytes(new_str, encoding='utf-8'))
+#     return
+
+
 from io import BytesIO
 from kaskadxml.kaskad_xml import KlogicXML
 
@@ -5,7 +29,7 @@ from kaskadxml.kaskad_xml import KlogicXML
 def shift_create(klogic_xml: KlogicXML) -> BytesIO:
     """Подсчет смещения адресов контроллеров"""
     shift_attr = klogic_xml.shift()
-    buffer = BytesIO()
+    txt = BytesIO()
 
     for l in shift_attr.all_lens:
         address = 0
@@ -18,5 +42,5 @@ def shift_create(klogic_xml: KlogicXML) -> BytesIO:
             )
             address = float(shift_attr.all_attrs[i].addr)
             new_str = f'Кол-во переменных = {l}. {shift_attr.all_attrs[i].name}. Смещение = {current_shift} \n'
-            buffer.write(bytes(new_str, encoding='utf-8'))
-    return buffer
+            txt.write(bytes(new_str, encoding='utf-8'))
+    return txt
